19236a59c4ac9074d9fb582d31cd8a229cf6a19c
karsvsdev <158139238+karsvsdev@users.noreply.github.com>
2024-01-30 16:25:37 +0530

    Update CourseController.java

--- src/main/java/io/javabrains/springbootstarter/course/CourseController.java
+++ src/main/java/io/javabrains/springbootstarter/course/CourseController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.Controller;
 
 import io.javabrains.springbootstarter.topic.Topic;
 
