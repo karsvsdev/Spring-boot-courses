e4fd73aa723bc8d357cd62b82da27516f203bc1c
karsvsdev <158139238+karsvsdev@users.noreply.github.com>
2024-01-30 16:25:06 +0530

    Update TopicController.java
    Rest Controller to controller

--- src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.Controller;
 
-@RestController
+@Controller
 public class TopicController {
 	
 	@Autowired
